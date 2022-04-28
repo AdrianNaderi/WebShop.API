4cc7d938ccd54019542eaa93f01bda061d1839f6
JoyWanjiku <76406254+JoyWanjiku@users.noreply.github.com>
2022-04-28 13:02:11 +0100

    added Review class entity + properties

---
+++ WebShop.API/Models/Entities/Review.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebShop.API.Models.Entities
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ReviewStars { get; set; } 
+        [Required]
+        public string Comment { get; set; } = null!;
+        [Required]
+        public string VoterName { get; set; } = null!;
+        [Required]
+        public string VoterEmail { get; set; } = null!;
+
+
+        [ForeignKey("product")] //this part can be changed if need be guys :)
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
