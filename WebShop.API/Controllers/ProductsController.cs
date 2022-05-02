4f08ca75203f05bcb047285d6e1e9f6ce9f8209d
EmilFrick <emil.a.frick@gmail.com>
2022-05-02 19:09:03 +0200

    Products Controller Added

---
+++ WebShop.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.API.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ProductsController : ControllerBase
+        {
+
+                public ProductsController()
+                {
+
+                }
+
+        }
+}
