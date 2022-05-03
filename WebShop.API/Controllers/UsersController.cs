f462ad1e25808de7d1eaee4a797f7cdf4da69080
EmilFrick <emil.a.frick@gmail.com>
2022-05-03 23:56:02 +0200

    #51 UserLogin Logic Completed

--- WebShop.API/Controllers/UsersController.cs
+++ WebShop.API/Controllers/UsersController.cs
@@ -7,59 +7,59 @@ using System.Threading.Tasks;
 
 namespace WebShop.API.Controllers
 {
-        [Route("api/[controller]")]
-        [ApiController]
-        public class UsersController : ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(ILoginService loginService, UserManager<IdentityUser> userManager)
         {
-                private readonly ILoginService _loginService;
-                private readonly UserManager<IdentityUser> _userManager;
-
-                public UsersController(ILoginService loginService, UserManager<IdentityUser> userManager)
-                {
-                        _loginService = loginService;
-                        _userManager = userManager;
-                }
-
-
-                [HttpPost("Login")]
-                public async Task<IActionResult> LoginUser(LoginUser loginUser)
-                {
-                        if (string.IsNullOrEmpty(loginUser.Payload))
-                                return BadRequest("Payload cannot be empty");
-
-                        var isEmail = loginUser.Payload.Contains('@');
-                        var result = string.Empty;
-
-                        if (isEmail)
-                                result = await _loginService.LoginWithEmailAsync(loginUser);
-                        else
-                                result = await _loginService.LoginWithUsernameAsync(loginUser);
-
-                        if (result is null)
-                                return BadRequest("Login Failed");
-
-                        return Ok(result);
-                }
-
-                [HttpPost]
-                public async Task<IActionResult> Register(UserRegisterModel model) 
-                {
-                    if (ModelState.IsValid)
-                    {
-                        var user = new IdentityUser()
-                        {
-                            UserName = model.UserName,
-                            Email = model.Email,
-                        };
-
-                        var registration = await _userManager.CreateAsync(user);
-                        if (registration.Succeeded)
-                        {
-                            return new OkObjectResult(user);
-                        }
-                    }
-
-                    return new BadRequestResult();
-                }
+            _loginService = loginService;
+            _userManager = userManager;
         }
+
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginUser(LoginUser loginUser)
+        {
+            if (string.IsNullOrEmpty(loginUser.Payload))
+                return BadRequest("Payload cannot be empty");
+
+            var isEmail = loginUser.Payload.Contains('@');
+            var result = string.Empty;
+
+            if (isEmail)
+                result = await _loginService.LoginWithEmailAsync(loginUser);
+            else
+                result = await _loginService.LoginWithUsernameAsync(loginUser);
+
+            if (result is null)
+                return BadRequest("Login Failed");
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                };
+
+                var registration = await _userManager.CreateAsync(user);
+                if (registration.Succeeded)
+                {
+                    return new OkObjectResult(user);
+                }
+            }
+
+            return new BadRequestResult();
+        }
+    }
 }
