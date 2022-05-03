9f980c66cba5ecc0557c91fe85432bd2cb3f225a
Adrian Naderi <Adrian.Naderi@hotmail.se>
2022-05-03 21:08:12 +0200

    Added UserRegister logic in controller

--- WebShop.API/Controllers/UsersController.cs
+++ WebShop.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.API.Models.ViewModels.UserModels;
 
 namespace WebShop.API.Controllers
 {
@@ -6,12 +8,32 @@ namespace WebShop.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        public UsersController()
-        {
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                };
 
+                var registration = await _userManager.CreateAsync(user);
+                if (registration.Succeeded)
+                {
+                    return new OkObjectResult(user);
+                }
+            }
 
+            return new BadRequestResult();
+        }
     }
 }
