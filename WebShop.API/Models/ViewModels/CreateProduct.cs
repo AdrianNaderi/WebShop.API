57449055e0ea96e40d70016d150619f3f288e085
tedhasserud <ted.hasserud@hotmail.se>
2022-05-13 16:16:06 +0200

    Change name from Brand to BrandId in CreateProduct class

--- WebShop.API/Models/ViewModels/CreateProduct.cs
+++ WebShop.API/Models/ViewModels/CreateProduct.cs
@@ -9,7 +9,7 @@ namespace WebShop.API.Models.ViewModels
                 public string Description { get; set; }
                 public string Color { get; set; }
                 public string Size { get; set; }
-                public string Brand { get; set; }
+                public string BrandId { get; set; }
                 public string Category { get; set; }
                 public bool OnSale { get; set; }
                 public int Quantity { get; set; }
