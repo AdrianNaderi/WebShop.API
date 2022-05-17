cacf8e875170995aea133a8e3c0f811618033b6e
Adrian Naderi <Adrian.Naderi@hotmail.se>
2022-05-18 00:49:35 +0200

    UnitTest imp For ReadSingelProduct(Service and Controller)

---
+++ WebShop.Tests/UnitTests/Controllers/ProductsController_Should.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.API.Controllers;
+using WebShop.API.Models.ViewModels.Product;
+using WebShop.API.Services;
+using WebShop.Tests.UnitTests.Fixtures;
+using Xunit;
+
+namespace WebShop.Tests.UnitTests.Controllers
+{
+    public class ProductsController_Should
+    {
+        [Fact]
+        public async Task GetProduct_OnSuccess_Return_Product()
+        {
+            // Arrange
+            var id = 2;
+            var mockProductService = new Mock<IProductService>();
+            var mockMapper = new Mock<IMapper>();
+            mockProductService.Setup(s => s.ReadSingleProductAsync(id))
+                .ReturnsAsync(await ProductFixtures.ReadSingleProductAsync(id));
+
+            var sut = new ProductsController(mockProductService.Object, mockMapper.Object);
+
+            // Act
+            var actionResult = await sut.GetProduct(id);
+            var result = actionResult.Result as OkObjectResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+            result.Value.Should().BeOfType<ProductViewModel>();
+        }
+    }
+}
---
+++ WebShop.Tests/UnitTests/Services/ProductService_Should.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.API.Models.ViewModels.Product;
+using WebShop.API.Services;
+using WebShop.Tests.UnitTests.Fixtures;
+using Xunit;
+
+namespace WebShop.Tests.UnitTests.Services
+{
+    public class ProductService_Should
+    {
+        [Fact]
+        public async Task ReadSingleProductAsync_Returns_Product()
+        {
+            // Arrange
+            var id = 2;
+            var sut = new Mock<IProductService>();
+            sut.Setup(s => s.ReadSingleProductAsync(id))
+                .ReturnsAsync(await ProductFixtures.ReadSingleProductAsync(id));
+
+            // Act
+            var result = await sut.Object.ReadSingleProductAsync(id);
+
+            // Assert
+            result.Should().BeOfType<ProductViewModel>();
+            result.Id.Should().Be(id);
+        }
+    }
+}
